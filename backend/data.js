8f514f6612201ecb4cffd436c5408b4dd7682d7a
0953344888 <jiya_qqq@hotmail.com>
2022-01-27 21:03:04 +0700

    push

--- backend/data.js
+++ backend/data.js
@@ -3,8 +3,8 @@ import bcrypt from "bcryptjs";
 const data = {
   users: [
     {
-      name: "Basir",
-      email: "admin@example.com",
+      name: "Jay",
+      email: "jay@hotmail.com",
       password: bcrypt.hashSync("1234", 8),
       isAdmin: true,
     },
