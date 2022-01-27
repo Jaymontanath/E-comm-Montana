1ed754e52d84f7422d6de021e4b7c9a2c3ef4a42
0953344888 <jiya_qqq@hotmail.com>
2022-01-27 17:43:49 +0700

    lesson 23 "finally data linked to the backend, takes me a long time too."

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,12 +1,36 @@
 import Axios from "axios";
 import {
+  USER_REGISTER_FAIL,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
   USER_SIGNIN_FAIL,
   USER_SIGNIN_REQUEST,
   USER_SIGNIN_SUCCESS,
   USER_SIGNOUT,
 } from "../constants/userConstants";
 
-////sign in request //
+export const register = (name, email, password) => async (dispatch) => {
+  dispatch({ type: USER_REGISTER_REQUEST, payload: { email, password } });
+  try {
+    const { data } = await Axios.post("/api/users/register", {
+      name,
+      email,
+      password,
+    });
+    dispatch({ type: USER_REGISTER_SUCCESS, payload: data });
+    dispatch({ type: USER_SIGNIN_SUCCESS, payload: data });
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_REGISTER_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
 export const signin = (email, password) => async (dispatch) => {
   dispatch({ type: USER_SIGNIN_REQUEST, payload: { email, password } });
   try {
@@ -15,7 +39,6 @@ export const signin = (email, password) => async (dispatch) => {
     localStorage.setItem("userInfo", JSON.stringify(data));
   } catch (error) {
     dispatch({
-      /////fail part catch error for sign in ///
       type: USER_SIGNIN_FAIL,
       payload:
         error.response && error.response.data.message
@@ -24,6 +47,7 @@ export const signin = (email, password) => async (dispatch) => {
     });
   }
 };
+
 export const signout = () => (dispatch) => {
   localStorage.removeItem("userInfo");
   localStorage.removeItem("cartItems");
