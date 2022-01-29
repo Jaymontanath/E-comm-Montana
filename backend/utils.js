b87e3d34b4d6ecbfaab9a66fb3e6972903219079
0953344888 <jiya_qqq@hotmail.com>
2022-01-29 12:32:03 +0700

    almost done

--- backend/utils.js
+++ backend/utils.js
@@ -34,4 +34,11 @@ export const isAuth = (req, res, next) => {
   } else {
     res.status(401).send({ message: "No Token" });
   }
+}; // is this user are Admin if not it's res invaliid admin token //
+export const isAdmin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401).send({ message: "Invalid Admin Token" });
+  }
 };
